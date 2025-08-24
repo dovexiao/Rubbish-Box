11f32dd8a208f5cad627b53adba2e101420f8d2d
Peter <2060988558@qq.com>
2025-08-24 21:22:16 +0530

    apk下载链接

--- src/utils/web.ts
+++ src/utils/web.ts
@@ -129,15 +129,15 @@ export const downloadApk = () => {
   const channelId = globalStore.channel;
   if (globalStore.isAndroid) {
     if (channelId === 'supbet') {
-      Linking.openURL('https://file.supbet001.com/apk/supbet.apk');
+      Linking.openURL('https://www.staticimg007.com.com/apk/supbet.apk');
     } else {
-      Linking.openURL(`https://file.supbet001.com/apk/supbet_${channelId}.apk
+      Linking.openURL(`https://www.staticimg007.com.com/apk/supbet_${channelId}.apk
       `);
     }
   }
   if (channelId === 'supbet') {
-    location.href = 'https://file.supbet001.com/apk/supbet.apk';
+    location.href = 'https://www.staticimg007.com/apk/supbet.apk';
   } else {
-    location.href = `https://file.supbet001.com/apk/supbet_${channelId}.apk`;
+    location.href = `https://www.staticimg007.com/apk/supbet_${channelId}.apk`;
   }
 };
