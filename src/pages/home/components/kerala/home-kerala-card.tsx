d3b088baa19e7e71560c7d8d7a5c5a8d382410d0
kf-wpc1 <143266374+kf-wpc1@users.noreply.github.com>
2025-08-27 16:18:49 +0800

    karala游戏列表样式调整

--- src/pages/home/components/kerala/home-kerala-card.tsx
+++ src/pages/home/components/kerala/home-kerala-card.tsx
@@ -1,11 +1,11 @@
 import React, {useMemo} from 'react';
 import {KeralaListItem} from '../../home.type';
 import Text from '@basicComponents/text';
-import {View} from 'react-native';
+import {View, ImageBackground} from 'react-native';
 import theme from '@/style';
 import LazyImage, {LazyImageBackground} from '@basicComponents/image';
 import {NativeTouchableOpacity} from '@basicComponents/touchable-opacity';
-import {goTo} from '@/utils';
+import {goTo, toPriceStr} from '@/utils';
 import dayjs from 'dayjs';
 import globalStore from '@/services/global.state';
 import {useTranslation} from 'react-i18next';
@@ -24,28 +24,16 @@ const HomeKeralaCard = ({
   imageHeight: number;
 }) => {
   const {i18n} = useTranslation();
-  // const remain = useMemo(() => {
-  //   const seconds = dayjs(item.drawDate).diff(dayjs(), 'second');
-  //   if (seconds < 0) {
-  //     return {hours: 0, minutes: 0, closed: true};
-  //   }
-  //   const hours = Math.floor(seconds / 3600);
-  //   const minutes = Math.floor(seconds / 60) % 60;
-  //   return {hours, minutes, closed: false};
-  // }, [item]);
+  // const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   const remain = useMemo(() => {
     const seconds = dayjs(item.drawDate).diff(dayjs(), 'second');
     if (seconds < 0) {
-      return {days: 0, hours: 0, minutes: 0, closed: true};
+      return {hours: 0, minutes: 0, closed: true};
     }
-
-    const days = Math.floor(seconds / 86400); // 一天86400秒
-    const hours = Math.floor((seconds % 86400) / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-
-    return {days, hours, minutes, closed: false};
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor(seconds / 60) % 60;
+    return {hours, minutes, closed: false};
   }, [item]);
-
   const lotteryName = useMemo(
     () =>
       (item.lotteryType?.indexOf('BUMPER') > -1 ? 'festival' : 'weekly') +
@@ -84,57 +72,112 @@ const HomeKeralaCard = ({
           theme.flex.alignStart,
           theme.flex.between,
           theme.padding.lrs,
-          theme.padding.topxs,
+          theme.padding.tops,
           theme.padding.btmxxs,
         ]}>
+        {/* <View style={[theme.flex.row, theme.fill.fillW, theme.flex.between]}>
+          <View style={[theme.margin.lefts]}>
+            <Text
+              fontSize={8}
+              blod
+              style={[
+                theme.font.white,
+                theme.padding.tbxxxs,
+                theme.padding.lrxs,
+                theme.borderRadius.s,
+                {backgroundColor: 'rgba(0, 0, 0, 0.30)'},
+              ]}>
+              {lotteryName}
+            </Text>
+          </View>
+
+          <View style={[theme.flex.col, theme.flex.alignEnd]}>
+            <Text fontSize={9} blod style={[theme.font.white]}>
+              {i18n.t('home.kerala.no')}
+            </Text>
+            <Text
+              fontSize={9}
+              blod
+              style={[
+                theme.font.white,
+                globalStore.isWeb ? theme.margin.topxxs : {},
+              ]}>
+              {item.issueNo}
+            </Text>
+          </View>
+        </View> */}
+
+        {/* <View
+          style={[
+            theme.padding.lrs,
+            theme.fill.fillW,
+            theme.flex.row,
+            theme.flex.centerByCol,
+            theme.flex.between,
+          ]}>
+          <View style={[theme.flex.col]}>
+            <Text white fontFamily="fontInterBold">
+              {toPriceStr(item.lotteryPrice, {currency: globalStore.currency})}
+            </Text>
+          </View>
+          <View style={[theme.flex.col, theme.flex.alignEnd]}>
+            <Text fontSize={9} white>
+              {weekDays[new Date(item.drawDate).getDay()]} {item.drawTime}
+            </Text>
+            <View style={[theme.flex.row, theme.flex.centerByCol]}>
+              <LazyImage
+                occupancy={'transparent'}
+                imageUrl={require('@assets/icons/home/hourglass.webp')}
+                width={9}
+                height={9}
+              />
+              <Text fontSize={9} white style={[theme.margin.leftxxs]}>
+                {remain.hours} hrs {remain.minutes} mins
+              </Text>
+            </View>
+          </View>
+        </View> */}
+        <ImageBackground
+          style={[
+            theme.flex.flex,
+            theme.flex.center,
+            {
+              width: 80,
+              height: 14,
+              position: 'absolute',
+              top: 0,
+              left: '50%',
+              transform: [{translateX: -40}],
+            },
+          ]}
+          source={require('@assets/icons/home/game-header.webp')}>
+          <Text fontSize={8} blod style={[theme.font.white]}>
+            {item.issueNo}
+          </Text>
+        </ImageBackground>
         <View
           style={[
             theme.flex.row,
-            theme.fill.fillW,
+            theme.flex.centerByCol,
             theme.flex.between,
-            {marginTop: 33},
+            theme.padding.lrs,
+            {
+              width: cardWidth,
+              height: cardWidth / 7,
+              backgroundColor: 'rgba(3,3,3,0.40)',
+              borderBottomLeftRadius: 10,
+              borderBottomRightRadius: 10,
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+            },
           ]}>
-          {/* 左边：售价 */}
-          <View>
-            <Text
-              fontSize={16}
-              blod
-              style={{color: '#FFD700', fontStyle: 'italic'}}>
-              ₹{item.sellMoney}
-            </Text>
-          </View>
-
-          {/* 右边：期号 */}
-          <View>
-            <Text
-              fontSize={16}
-              blod
-              style={{color: '#FFFFFFAA', fontStyle: 'italic'}}>
-              No. {item.issueNo}
-            </Text>
-          </View>
-        </View>
-
-        <View style={[theme.fill.fillW, theme.flex.col, theme.flex.between]}>
-          {/* 上方：价格，靠左、向上 */}
-          <View
-            style={{alignItems: 'flex-start', position: 'relative', top: -3}}>
-            <Text white fontSize={14} fontFamily="fontInterBold">
-              {item.lotteryPrice}
-            </Text>
-          </View>
-
-          <View style={[theme.flex.col, {alignItems: 'center'}]}>
-            <Text
-              fontSize={18}
-              fontFamily="fontInterBold" // 加粗
-              style={{color: '#FFD700'}} // 高亮金黄（可自定义）
-            >
-              {remain.days > 0
-                ? `${remain.days} days ${remain.hours} hrs`
-                : `${remain.hours} hrs ${remain.minutes} mins`}
-            </Text>
-          </View>
+          <Text white fontFamily="fontInterBold">
+            {toPriceStr(item.sellMoney, {currency: globalStore.currency})}
+          </Text>
+          <Text fontSize={9} white style={[theme.margin.leftxxs]}>
+            {remain.hours} hrs {remain.minutes} mins
+          </Text>
         </View>
       </LazyImageBackground>
 
