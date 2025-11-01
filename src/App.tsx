deb15cfb550ad5fa89531538dc82a081f1e66118
Peter <2060988558@qq.com>
2025-11-01 11:41:15 +0530

    ad token 替换

--- src/App.tsx
+++ src/App.tsx
@@ -425,7 +425,7 @@ function App(): JSX.Element {
       // ios的在TS文件中已调用start方法，这边在调用就会多次触发
       const {Adjust, AdjustConfig} = require('react-native-adjust');
       const adjustConfig = new AdjustConfig(
-        'kkwoiyprm0ow',
+        '33z3sllv0cqo',
         __DEV__
           ? AdjustConfig.EnvironmentSandbox
           : AdjustConfig.EnvironmentProduction,
@@ -441,7 +441,7 @@ function App(): JSX.Element {
     } else {
       const Adjust = require('@adjustcom/adjust-web-sdk');
       Adjust.initSdk({
-        appToken: 'kkwoiyprm0ow',
+        appToken: '33z3sllv0cqo',
         environment: __DEV__ ? 'sandbox' : 'production', // 或'production'
         logLevel: 'verbose', // 可选
       });
