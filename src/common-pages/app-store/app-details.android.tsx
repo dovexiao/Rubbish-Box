3ad080cf1ea9577afdc4977d5a08d0c4468cd2b6
Lantz <lantzshaw@163.com>
2025-11-14 19:45:45 +0800

    fix: 页面特定平台区分

---
+++ src/common-pages/app-store/app-details.android.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export interface AppDetailsProps {}
+
+/**
+ * 在 app 中不需要此页面，占位处理
+ */
+const AppDetails = ({}: AppDetailsProps) => {
+  return <View />;
+};
+
+export default AppDetails;
